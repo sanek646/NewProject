7084d006802ff6646da49c68baf6aafcd6b3d0f0
sanek646 <646sanek@rambler.ru>
2021-07-20 10:56:17 +0300

    f

---
+++ NewProjekt/src/main/java/com/example/NewProjekt/NewProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewProjektApplication.class, args);
+	}
+
+}
---
+++ NewProjekt/src/test/java/com/example/NewProjekt/NewProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
